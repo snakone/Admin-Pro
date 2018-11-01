2b209bc5d705f67492100bbf34a905821bfb1c28
Snakone <snak_one@hotmail.com>
2018-11-01 19:07:39 +0100

    Assets added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SignInComponent } from './login/signin/signin.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafics1Component } from './pages/grafics1/grafics1.component';
+import { Error404Component } from './error404/error404.component';
+import { RegisterComponent } from './login/register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+   {
+     path: '',
+     component: PagesComponent,
+     children: [
+       { path: 'dashboard', component: DashboardComponent },
+       { path: 'progress', component: ProgressComponent },
+       { path: 'graficas1', component: Grafics1Component },
+     ]
+   },
+
+   { path: 'login', component: SignInComponent },
+   { path: 'register', component: RegisterComponent },
+   { path: '**', component: Error404Component },
+   // { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
---
+++ src/app/pages/grafics1/grafics1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grafics1',
+  templateUrl: './grafics1.component.html',
+  styles: []
+})
+
+export class Grafics1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
